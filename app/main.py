a4760c400972d6809141d29f424bcb78f9fefcb8
JosefX7 <hedhili.youssef@gmail.com>
2021-08-25 17:00:45 -0400

    fixed requiremnts

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def hello_world():
     return render_template('hello_world.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0:5000', debug=True)
+    app.run(port=8000, host='0.0.0.0', debug=True)
