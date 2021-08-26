0b55dc9211dd9bd2a28ad14f8764545d27b33234
JosefX7 <hedhili.youssef@gmail.com>
2021-08-26 11:32:35 +0200

    fixed requiremnts

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ import unittest
 class test_hello_world(unittest.TestCase):
     def test_hello_world(self):
         result = requests.get('http://localhost:5000/hello_world', verify=False).text
+        print(result)
         test = result.find("Hello World!") != -1
         self.assertTrue(test)
 
